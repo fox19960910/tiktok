aa892461cd6905fb8721af73ffa82aa839cfc0fb
Sotatek-Tungle2 <tung.le2@sotatek.com>
2022-08-26 16:47:49 +0700

    base: router

---
+++ src/pages/Following/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Following.scss';
+const Following = () => {
+    return <div>Following</div>;
+};
+
+export default Following;
---
+++ src/pages/Upload/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Upload.scss';
+const Upload = () => {
+    return <div>Upload</div>;
+};
+
+export default Upload;
